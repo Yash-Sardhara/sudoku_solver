6fe67ab6ef5c20fd1eb99bab38df27c717a2a5f3
Yash_Sardhara <yash.sardhara1@gmail.com>
2020-01-01 03:28:02 -0800

    GUI basic input implemented SudokuSearchSolver class created

--- PythonSudokuSolver/SudokuGUI.py
+++ PythonSudokuSolver/SudokuGUI.py
@@ -1,32 +1,62 @@
 from tkinter import *
+from math import sqrt
+from SudokuSearchSolver import SudokuSearchSolver
+from SudokuSearchSolver import print_board
 
 SIZE = 9
 
 
 class Cell:
-    board = [[0]*SIZE]*SIZE
-
     def __init__(self, gui_root, row, column):
         self.row = row
         self.column = column
         self.input_cell = Entry(gui_root, width=5)
         self.input_cell.grid(row=self.row, column=self.column)
 
-    def save_number(self):
+    def get_number(self):
         input_number = self.input_cell.get()
         if input_number:
-            self.board[self.row][self.column] = input_number
+            return input_number
+        else:
+            return 0
+
+    def clear(self):
+        self.input_cell.delete(0, END)
+
+
+def solve_board(cell_grid, size):
+    sudoku_board = []
+
+    for i in range(size):
+        sudoku_board_row = []
+        for j in range(size):
+            sudoku_board_row.append(cell_grid[i][j].get_number())
+        sudoku_board.append(sudoku_board_row)
+
+    SudokuSearchSolver.search_solve(sudoku_board, [0, 0])
+    print_board(sudoku_board)
+
+
+def clear_grid(cell_grid, size):
+    for i in range(size):
+        for j in range(size):
+            cell_grid[i][j].clear()
 
 
 root = Tk()
-cell_grid = [[None]*9]*9
-button_solve = Button(root, text="solve")
-
 root.title("Sudoku Solver")
-button_solve.grid(row=9, column=6)
-for i in range(9):
-    for j in range(9):
-        cell_grid[i][j] = Cell(root, i, j)
 
+cell_grid = []
+for i in range(9):
+    cell_grid_row = []
+    for j in range(9):
+        cell_grid_row.append(Cell(root, i, j))
+    cell_grid.append(cell_grid_row)
+
+button_solve = Button(root, text="solve", command=lambda: solve_board(cell_grid, SIZE))\
+    .grid(row=SIZE, column=int((sqrt(SIZE)-1)*sqrt(SIZE)), columnspan=int((sqrt(SIZE)-1)*sqrt(SIZE)))
+
+button_clear = Button(root, text="Clear", command=lambda: clear_grid(cell_grid, SIZE))\
+    .grid(row=SIZE, column=int((sqrt(SIZE)-2)*sqrt(SIZE)), columnspan=int((sqrt(SIZE)-1)*sqrt(SIZE)))
 
 root.mainloop()
--- PythonSudokuSolver/SudokuSearchSolver.py
+++ PythonSudokuSolver/SudokuSearchSolver.py
@@ -1,7 +1,81 @@
 from math import sqrt
 from time import time
 
-SIZE = 9
+
+class SudokuSearchSolver:
+    SIZE = 9
+
+    @staticmethod
+    def check_row(board, row, column):
+        for i in range(SudokuSearchSolver.SIZE):
+            if i != column:
+                if board[row][i] == board[row][column]:
+                    return False
+            else:
+                if board[row][column] > SudokuSearchSolver.SIZE or board[row][column] <= 0:
+                    return False
+        return True
+
+    @staticmethod
+    def check_column(board, row, column):
+        for i in range(SudokuSearchSolver.SIZE):
+            if i != row:
+                if board[i][column] == board[row][column]:
+                    return False
+            else:
+                if board[row][column] > SudokuSearchSolver.SIZE or board[row][column] <= 0:
+                    return False
+        return True
+
+    @staticmethod
+    def check_box(board, row, column):
+        box_row = row // sqrt(SudokuSearchSolver.SIZE)
+        box_column = column // sqrt(SudokuSearchSolver.SIZE)
+
+        for i in range(int(box_row * sqrt(SudokuSearchSolver.SIZE)),
+                       int((box_row + 1) * sqrt(SudokuSearchSolver.SIZE))):
+            for j in range(int(box_column * sqrt(SudokuSearchSolver.SIZE)),
+                           int((box_column + 1) * sqrt(SudokuSearchSolver.SIZE))):
+                if i != row or j != column:
+                    if board[i][j] == board[row][column]:
+                        return False
+                else:
+                    if board[row][column] > SudokuSearchSolver.SIZE or board[row][column] <= 0:
+                        return False
+        return True
+
+    @staticmethod
+    def check_all(board, row, column):
+        return SudokuSearchSolver.check_box(board, row, column) \
+               and SudokuSearchSolver.check_row(board, row, column) \
+               and SudokuSearchSolver.check_column(board, row, column)
+
+    @staticmethod
+    def search_solve(board, position):
+        if position[0] == SudokuSearchSolver.SIZE:
+            return True
+
+        if board[position[0]][position[1]]:
+            position[1] += 1
+            if position[1] == SudokuSearchSolver.SIZE:
+                position[0], position[1] = position[0] + 1, 0
+                return SudokuSearchSolver.search_solve(board, position)
+            return SudokuSearchSolver.search_solve(board, position)
+
+        for i in range(1, SudokuSearchSolver.SIZE + 1):
+            row, column = position
+            board[row][column] = i
+            if SudokuSearchSolver.check_all(board, row, column):
+                column += 1
+                if column == SudokuSearchSolver.SIZE:
+                    row, column = row + 1, 0
+                if SudokuSearchSolver.search_solve(board, [row, column]):
+                    return True
+
+        board[position[0]][position[1]] = 0
+        return False
+
+
 '''
 SUDOKU_BOARD = [[1, 0, 6, 0, 0, 2, 3, 0, 0],
                 [0, 5, 0, 0, 0, 6, 0, 9, 1],
@@ -24,73 +98,7 @@ SUDOKU_BOARD = [[8, 0, 0, 4, 0, 6, 0, 0, 7],
                 [0, 0, 1, 0, 0, 0, 0, 0, 0],
                 [3, 0, 0, 9, 0, 2, 0, 0, 5]]
 
-def check_row(board, row, column):
-    for i in range(SIZE):
-        if i != column:
-            if board[row][i] == board[row][column]:
-                return False
-        else:
-            if board[row][column] > SIZE or board[row][column] <= 0:
-                return False
-    return True
-
-
-def check_column(board, row, column):
-    for i in range(SIZE):
-        if i != row:
-            if board[i][column] == board[row][column]:
-                return False
-        else:
-            if board[row][column] > SIZE or board[row][column] <= 0:
-                return False
-    return True
-
-
-def check_box(board, row, column):
-    box_row = row//sqrt(SIZE)
-    box_column = column//sqrt(SIZE)
-
-    for i in range(int(box_row*sqrt(SIZE)), int((box_row + 1)*sqrt(SIZE))):
-        for j in range(int(box_column * sqrt(SIZE)), int((box_column + 1) * sqrt(SIZE))):
-            if i != row or j != column:
-                if board[i][j] == board[row][column]:
-                    return False
-            else:
-                if board[row][column] > SIZE or board[row][column] <= 0:
-                    return False
-    return True
-
-
-def check_all(board, row, column):
-    return check_box(board, row, column)\
-           and check_row(board, row, column)\
-           and check_column(board, row, column)
-
-
-def search_solve(board, position):
-    if position[0] == SIZE:
-        return True
-
-    if board[position[0]][position[1]]:
-        position[1] += 1
-        if position[1] == SIZE:
-            position[0], position[1] = position[0] + 1, 0
-            return search_solve(board, position)
-        return search_solve(board, position)
-
-    for i in range(1, SIZE+1):
-        row, column = position
-        board[row][column] = i
-        if check_all(board, row, column):
-            column += 1
-            if column == SIZE:
-                row, column = row+1, 0
-            if search_solve(board, [row, column]):
-                return True
-
-    board[position[0]][position[1]] = 0
-    return False
-
+SIZE = 9
 
 def print_board(board):
     for i in range(SIZE):
@@ -103,11 +111,9 @@ def print_board(board):
 def execute():
     start_time = time()
     solved_board = SUDOKU_BOARD
-    search_solve(solved_board, [0, 0])
+    SudokuSearchSolver.search_solve(solved_board, [0, 0])
     print_board(SUDOKU_BOARD)
     print()
     print_board(solved_board)
     print("Execution Time: ", time()-start_time)
 
-
-execute()
